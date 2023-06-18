d5ce8165d866d6a7eed0ec2b815c11735e817d42
Anuj Mishra <anujmishra282003@gmail.com>
2023-06-18 17:56:41 +0530

    k

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import json
 OPENAI_API_KEY = "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"
 def translate_code(code, source_language, target_language):
     prompt = f"Translate this code from {source_language} to {target_language}:\n\n{code}"
-    
+
     headers = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {OPENAI_API_KEY}"
@@ -19,28 +19,32 @@ def translate_code(code, source_language, target_language):
         "stop": None,
         "log_level": "info"
     }
-    
+
     response = requests.post(
         "https://api.openai.com/v1/engines/davinci-codex/completions",
         headers=headers,
         data=json.dumps(data)
     )
-    
-    translated_code = response.json()["choices"][0]["text"].strip()
-    
+
+    choices = response.json()["choices"]
+    if choices:
+        translated_code = choices[0]["text"].strip()
+    else:
+        translated_code = "No translation found."
+
     return translated_code
 
 def main():
     st.title("Programming Language Translator")
-    
+
     source_language = st.selectbox("Source Language:", ["Python", "Java", "C", "C++"])
     target_language = st.selectbox("Target Language:", ["Haskell", "JavaScript", "C++"])
     code = st.text_area("Code:")
-    
+
     if st.button("Translate"):
         translated_code = translate_code(code, source_language, target_language)
         st.code(translated_code, language=target_language)
         st.button("Copy to Clipboard", translated_code)
-    
+
 if __name__ == "__main__":
     main()
